324dde2550170e9ba6c12054b1418cc514a9c683
Mika Cohen <mjc@kernel.org>
2026-06-05 11:07:43 -0600

    fix: detach virtual media before image changes

--- server/service/storage/image.go
+++ server/service/storage/image.go
@@ -1,28 +1,20 @@
 package storage
 
 import (
-	"fmt"
 	"os"
-	"os/exec"
 	"path/filepath"
-	"strconv"
 	"strings"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 
 	"NanoKVM-Server/proto"
 	"NanoKVM-Server/service/hid"
+	"NanoKVM-Server/service/usb"
 )
 
 const (
 	imageDirectory = "/data"
-	imageNone      = "/dev/mmcblk0p3"
-	cdromFlag      = "/sys/kernel/config/usb_gadget/g0/functions/mass_storage.disk0/lun.0/cdrom"
-	mountDevice    = "/sys/kernel/config/usb_gadget/g0/functions/mass_storage.disk0/lun.0/file"
-	inquiryString  = "/sys/kernel/config/usb_gadget/g0/functions/mass_storage.disk0/lun.0/inquiry_string"
-	roFlag         = "/sys/kernel/config/usb_gadget/g0/functions/mass_storage.disk0/lun.0/ro"
 )
 
 func (s *Service) GetImages(c *gin.Context) {
@@ -63,84 +55,10 @@ func (s *Service) MountImage(c *gin.Context) {
 		return
 	}
 
-	// cdrom and ro flag
-	// set to 0 when unmount image
-	// set to 1 when mount image and the CD-ROM is enabled
-	if req.File == "" || req.Cdrom {
-		flag := "0"
-		if req.File != "" && req.Cdrom {
-			flag = "1"
-		}
-
-		// unmount
-		if err := os.WriteFile(mountDevice, []byte("\n"), 0o666); err != nil {
-			log.Errorf("unmount file failed: %s", err)
-			rsp.ErrRsp(c, -2, "unmount image failed")
-			return
-		}
-
-		// ro flag
-		if err := os.WriteFile(roFlag, []byte(flag), 0o666); err != nil {
-			log.Errorf("set ro flag failed: %s", err)
-			rsp.ErrRsp(c, -2, "set ro flag failed")
-			return
-		}
-
-		// cdrom flag
-		if err := os.WriteFile(cdromFlag, []byte(flag), 0o666); err != nil {
-			log.Errorf("set cdrom flag failed: %s", err)
-			rsp.ErrRsp(c, -2, "set cdrom flag failed")
-			return
-		}
-	}
-
-	inquiryVen := "NanoKVM"
-	inquiryPrd := "USB Mass Storage"
-	inquiryVer := 0x0520
-	if req.Cdrom {
-		inquiryPrd = "USB CD/DVD-ROM"
-	}
-	inquiryData := fmt.Sprintf("%-8s%-16s%04x", inquiryVen, inquiryPrd, inquiryVer)
-
-	if err := os.WriteFile(inquiryString, []byte(inquiryData), 0o666); err != nil {
-		log.Errorf("set inquiry %s failed: %s", inquiryData, err)
-		rsp.ErrRsp(c, -2, "set inquiry failed")
-		return
-	}
-
-	// mount
-	image := req.File
-	if image == "" {
-		image = imageNone
-	}
-
-	if err := os.WriteFile(mountDevice, []byte(image), 0o666); err != nil {
-		log.Errorf("mount file %s failed: %s", image, err)
+	if err := usb.SetLUNImage(hid.GetHid(), req.File, req.Cdrom); err != nil {
+		log.Errorf("set image %q cdrom=%t failed: %s", req.File, req.Cdrom, err)
 		rsp.ErrRsp(c, -2, "mount image failed")
 		return
-	}
-
-	h := hid.GetHid()
-	h.Lock()
-	h.CloseNoLock()
-	defer func() {
-		h.OpenNoLock()
-		h.Unlock()
-	}()
-
-	// reset usb
-	commands := []string{
-		"echo > /sys/kernel/config/usb_gadget/g0/UDC",
-		"ls /sys/class/udc/ | cat > /sys/kernel/config/usb_gadget/g0/UDC",
-	}
-
-	for _, command := range commands {
-		err := exec.Command("sh", "-c", command).Run()
-		if err != nil {
-			rsp.ErrRsp(c, -2, "execute command failed")
-			return
-		}
-		time.Sleep(100 * time.Millisecond)
 	}
 
 	rsp.OkRsp(c)
@@ -150,15 +68,10 @@ func (s *Service) MountImage(c *gin.Context) {
 func (s *Service) GetMountedImage(c *gin.Context) {
 	var rsp proto.Response
 
-	content, err := os.ReadFile(mountDevice)
+	image, err := usb.MountedImage()
 	if err != nil {
 		rsp.ErrRsp(c, -2, "read failed")
 		return
-	}
-
-	image := strings.ReplaceAll(string(content), "\n", "")
-	if image == imageNone {
-		image = ""
 	}
 
 	data := &proto.GetMountedImageRsp{
@@ -171,21 +84,14 @@ func (s *Service) GetMountedImage(c *gin.Context) {
 func (s *Service) GetCdRom(c *gin.Context) {
 	var rsp proto.Response
 
-	content, err := os.ReadFile(cdromFlag)
+	cdrom, err := usb.CDROMFlag()
 	if err != nil {
 		rsp.ErrRsp(c, -1, "read failed")
 		return
 	}
 
-	flag := strings.ReplaceAll(string(content), "\n", "")
-	flatInt, err := strconv.ParseInt(flag, 10, 64)
-	if err != nil {
-		rsp.ErrRsp(c, -2, "parse failed")
-		return
-	}
-
 	data := &proto.GetCdRomRsp{
-		Cdrom: flatInt,
+		Cdrom: cdrom,
 	}
 
 	rsp.OkRspWithData(c, data)
