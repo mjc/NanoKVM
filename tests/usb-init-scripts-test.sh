db392a8b8473947a406af91d9674483c58b64797
Mika Cohen <mjc@kernel.org>
2026-06-06 14:12:47 -0600

    fix: let data disk win over legacy media backing

--- tests/usb-init-scripts-test.sh
+++ tests/usb-init-scripts-test.sh
@@ -394,6 +394,20 @@ test_normal_legacy_media_backing_is_no_media(){
     assert_contains "${g}/functions/${USB_MASS_STORAGE_FUNC}/lun.0/inquiry_string" "USB Mass Storage" "legacy media inquiry"
 }
 
+test_normal_data_disk_wins_over_legacy_media_backing(){
+    base=$(new_env)
+    printf '%s' "${USB_DATA_DISK_BACKING}" > "${base}/boot/usb.media0"
+    touch "${base}/boot/usb.disk0"
+    run_start "${NORMAL_SCRIPT}" "${base}"
+    g="${base}/gadget/g0"
+
+    assert_link "${g}/configs/c.1/${USB_MASS_STORAGE_FUNC}" "functions/${USB_MASS_STORAGE_FUNC}"
+    assert_text_bytes "${g}/functions/${USB_MASS_STORAGE_FUNC}/lun.0/file" "${USB_DATA_DISK_BACKING}"
+    assert_eq "$(cat "${g}/functions/${USB_MASS_STORAGE_FUNC}/lun.0/ro")" "0" "legacy media data disk ro flag"
+    assert_eq "$(cat "${g}/functions/${USB_MASS_STORAGE_FUNC}/lun.0/cdrom")" "0" "legacy media data disk cdrom flag"
+    assert_contains "${g}/functions/${USB_MASS_STORAGE_FUNC}/lun.0/inquiry_string" "USB Data Disk" "legacy media data disk inquiry"
+}
+
 test_normal_mounted_image_and_network(){
     base=$(new_env)
     printf '%s' "${USB_TEST_IMAGE}" > "${base}/boot/usb.media0"
@@ -505,6 +519,7 @@ test_normal_disable_hid_removes_hid_functions
 test_normal_media_wins_over_data_disk
 test_normal_data_disk_uses_mass_storage_slot
 test_normal_legacy_media_backing_is_no_media
+test_normal_data_disk_wins_over_legacy_media_backing
 test_normal_mounted_image_and_network
 test_normal_mounted_cdrom_image_and_network
 test_hid_only_descriptors_and_no_wake
